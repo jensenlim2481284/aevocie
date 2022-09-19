9b82944e36925014c659feb632acbfc9c52e17fa
unknown <lim2481284@gmail.com>
2022-09-20 05:32:04 +0800

    Complete audiance app

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,17 +1,19 @@
 
-
 import Modal from 'react-modal';
+import Slider from "react-slick";
 import { Web3Storage } from 'web3.storage'
 import network from "./configs/network";
 import useAeternitySDK from './hooks/useAeternitySDK';
-import { MutableRefObject, useEffect, useRef, useState } from 'react';
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import { AeSdkAepp, AE_AMOUNT_FORMATS, AeSdk, Node, MemoryAccount } from '@aeternity/aepp-sdk';
 
 import './App.css';
 import logo from './assets/logo.png';
 import logoBG from './assets/logo.gif';
+import "slick-carousel/slick/slick.css";
 import Circle1 from "./assets/circle1.png";
 import Circle2 from "./assets/circle2.png";
+import "slick-carousel/slick/slick-theme.css";
 import logoBody from './assets/logo_body.png';
 import EncodedACI from './assets/aci/aci.json';
 import HeaderImage from "./assets/landingbg.png";
@@ -29,11 +31,21 @@ const App = () => {
 	const [status, setStatus] = useState(WalletConnectionStatus.Connecting)
 
 	// Smart contract state
-    const voiceArr = [<></>];
+    const voiceArr = [];
     const ACIJson = JSON.stringify(EncodedACI);
 	const [contract, setContract] = useState<any>("")
 	const [voiceRecords, setVoiceRecords] = useState<any>("")
 	const [voiceArrCheck, setVoiceArrCheck] = useState<string>(null)
+
+    // Slider setting 
+    let sliderRef = React.useRef(null);
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
 
     // Voice setting 
     let SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
@@ -50,11 +62,13 @@ const App = () => {
     recognition.maxAlternatives = 1;
 
     // Modal state 
+    const [msg, setMsg] = useState<string>('');
     const [modalIsOpen, setIsOpen] = useState(true);
+    const [answerModal, setAnswerModal] = useState(false);
 
     // Modal function 
-    function openModal() {  setIsOpen(true);}
-    function closeModal() {  setIsOpen(false);}
+    function openModal() {  setIsOpen(true); setAnswerModal(true);}
+    function closeModal() {  setIsOpen(false); setAnswerModal(false);}
 
     // Smart contract method - get voice records 
 	const getVoiceRecords = async function () {
@@ -88,6 +102,22 @@ const App = () => {
         }
     }   
 
+    // Answer question
+	const answerVoiceQuestion = async function (id:string, ans:string) {     
+        console.log(id);   
+        console.log(ans.toUpperCase());   
+        let result = (await contract.methods.answer_question(parseInt(id), ans.toUpperCase()) ).decodedResult;
+        console.log(result);
+        if(result) setMsg("It's correct !");
+        else setMsg("Aww... try again tomorrow");
+        setTimeout(() => {
+            setAnswerModal(false);
+        }, 1000);
+        updateAccountBalance();
+     }
+
+
+
     // Check ae client and fetch wallet account details 
 	useEffect(() => {
 		if (clientReady && client) {
@@ -108,12 +138,48 @@ const App = () => {
 
     if(voiceRecords){
 
-        // Start recognize voice
+        // Start recognize voice & voice control handling
         recognition.start()
         recognition.onresult = (event) => {
-            //handle result in here
+  
             let word = event.results[0][0].transcript
-            console.log(word);
+            let audio =  document.querySelector(
+                '.slick-current .audio-control',
+              ) as HTMLAudioElement  | null;
+            let a = sliderRef?.current;
+
+            switch (word) {
+                case 'play':
+                    audio.play();
+                    break;
+                case 'stop':
+                    audio.pause();
+                    break;
+                case 'repeat':
+                    audio.load();
+                    audio.play();
+                    break;
+                case 'next':
+                    if(a) a?.slickNext();
+                    break;
+                case 'previous':
+                    if(a) a?.slickPrev();
+                    break;
+                default:
+                    console.log(word);
+                    if(word == 'see') word = 'c';
+                    if(word == 'seen') word = 'c';
+                    if(word == 'seek') word = 'c';
+                    if(word !=='c' && word !=='b' && word !=='a' && word !=='d' && word !=='e' && word !=='f') return false;
+                    let input =  document.querySelector(
+                        '.slick-current .voiceID',
+                    ) as HTMLInputElement  | null;
+                    setMsg('Validating Answer');
+                    setAnswerModal(true);
+                    answerVoiceQuestion(input.value, word);
+                    break;
+            }
+
             recognition.onerror = function (event) {};
             recognition.onend = function() {
                 recognition.start();
@@ -125,13 +191,12 @@ const App = () => {
                 setVoiceArrCheck('1');
             voiceArr.push(
               <div className='voice-item' key={index}>
-                    <audio controls>
-                    <source src={record.voice_link} type="audio/mpeg"/>
-                    Your browser does not support the audio element.
-                    </audio>
+                    <input type='hidden' className='voiceID' value={parseInt(record.id)}/>
                     <div> <p> Reward </p> <b> Æ {parseFloat(record.reward)/ aenumber} </b></div>
-                    <div> <p> Max Reward </p> <b> Æ {parseFloat(record.max_reward)/ aenumber} </b></div>
-                    <div> <p> Answer </p> <b>{record.answer}</b></div>
+                    <audio controls className='audio-control'>
+                        <source src={record.voice_link} type="audio/mpeg"/>
+                        Your browser does not support the audio element.
+                    </audio>
               </div>
             );
         });
@@ -162,13 +227,40 @@ const App = () => {
                         <div id='voiceRecords'>
                             <div id='voiceArrsection'>
                                 {voiceArrCheck === null ? 
-                                    <b id='noAd'> There is no ad content yet. Click on Create Ads button to create new ad content </b>
-                                    : voiceArr
+                                    <b id='noAd'> There is no ad content yet.  </b>
+                                    : <Slider  ref={sliderRef} {...settings}> {voiceArr}  </Slider>
                                 }
                             </div>
+                          
                         </div>
+                        
                     </div>
                 </div>
+                <Modal closeTimeoutMS={500} isOpen={modalIsOpen} onRequestClose={closeModal}>
+                    
+                    <h2> Quick Walkthrough </h2>
+                    <p> You have to use voice input to control the system. The following is a list of Æ Voice command  : </p>
+                    <p className='l'>
+                            - play : to start reading the ads content to the audience <br/>
+                            - repeat : to repeat reading the ads content <br/>
+                            - stop : pause the reading <br/>
+                            - next : next ads content <br/>
+                            - previous : previous ads content <br/>
+                            - A - F  : to answer multiple choice question  
+                    </p>
+                    <div className='c'>
+                        <button onClick={() => setIsOpen(false)}> Got it </button>
+                    </div>
+                </Modal>
+                <Modal closeTimeoutMS={500} isOpen={answerModal} onRequestClose={closeModal}>
+                    <div className="span-container">
+                        <span className="one"></span>
+                        <span className="two"></span>
+                        <span className="three"></span>
+                        <span className="four"></span>
+                    </div>
+                    <h3> {msg} </h3>
+                </Modal>
             </>
         );
 
